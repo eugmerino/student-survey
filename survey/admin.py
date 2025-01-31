ef6a66024bddde8c7512d1ef1fa4e8f63bc3d4fa
Tito Merino <tiaguilar@stefanini.com>
2025-01-31 13:56:07 -0600

    Refactor survey admin

--- survey/admin.py
+++ survey/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from .models import Survey, ResponseGroup, Question, Option, Campaign, Assignment
 
 
+class QuestionInline(admin.TabularInline):
+    model = Question
+    list_filter = ('survey', 'response_group')
+    search_fields = ('question',)
+    autocomplete_fields = ('survey', 'response_group')
+    extra = 1  # Número de preguntas vacías para agregar
+
 @admin.register(Survey)
 class SurveyAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name', 'objective')
+    inlines = [QuestionInline]
 
 
 @admin.register(ResponseGroup)
@@ -14,17 +22,8 @@ class ResponseGroupAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question', 'survey', 'response_group')
-    list_filter = ('survey', 'response_group')
-    search_fields = ('question',)
-    autocomplete_fields = ('survey', 'response_group')
-
-
 @admin.register(Option)
 class OptionAdmin(admin.ModelAdmin):
-    list_display = ('text', 'response_group')
     list_filter = ('response_group',)
     search_fields = ('text',)
     autocomplete_fields = ('response_group',)
@@ -32,13 +31,11 @@ class OptionAdmin(admin.ModelAdmin):
 
 @admin.register(Campaign)
 class CampaignAdmin(admin.ModelAdmin):
-    list_display = ('name', 'start_date', 'end_date', 'survey')
     search_fields = ('name', 'description')
     list_filter = ('start_date', 'end_date')
 
 
 @admin.register(Assignment)
 class AssignmentAdmin(admin.ModelAdmin):
-    list_display = ('campaign', 'user', 'student')
     list_filter = ('campaign', 'user', 'student')
     search_fields = ('campaign__name', 'user__username', 'student__name')
