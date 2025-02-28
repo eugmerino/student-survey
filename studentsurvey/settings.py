c892acdb17677447514ecbb7db0a2f56323fb556
Tito Merino <tiaguilar@stefanini.com>
2025-02-28 16:38:42 -0600

    Add postgres database

--- studentsurvey/settings.py
+++ studentsurvey/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,8 +82,12 @@ WSGI_APPLICATION = 'studentsurvey.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_NAME", "postgres"),
+        'USER': os.getenv("POSTGRES_USER", "postgres"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD", "postgres"),
+        'HOST': 'db',  # Este es el nombre del servicio de la base de datos en el docker-compose
+        'PORT': '5432',  # Puerto por defecto de PostgreSQL
     }
 }
 
